52811df9997b9070db94c229b25528f86032af15
rahulgeorg <rahulgeorge1999@gmail.com>
2024-02-05 17:21:09 +0530

    banner updation on progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,37 @@
 $('.fm-banner__posters').slick({
-    prevArrow: $('.fm-banner__left-arw'),
-    nextArrow:$('.fm-banner__right-arw'),
-    dots: true,
+  prevArrow: $('.fm-banner__left-arw'),
+  nextArrow:$('.fm-banner__right-arw'),
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
