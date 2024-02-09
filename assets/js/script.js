941d938041aa1e7b2b03b499d7229e1ee3966738
Afsal P <palliyalafsal@gmail.com>
2024-02-09 11:59:28 +0530

    testimonials organism done

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,34 @@ $('.fm-banner__posters').slick({
     nextArrow:$('.fm-banner__right-arw'),
     dots: true,
 });
+$('.fm-testimonials__outer').slick({
+    prevArrow: $('.fm-testimonials__ltarw'),
+    nextArrow:$('.fm-testimonials__rtarw'),
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
