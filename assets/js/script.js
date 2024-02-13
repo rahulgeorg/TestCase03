8226213eb2c52ee2408f484409311b4ee1484cc5
rahulgeorg <rahulgeorge1999@gmail.com>
2024-02-13 09:31:38 +0530

    footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,4 @@
 $('.fm-banner__posters').slick({
-    prevArrow: $('.fm-banner__left-arw'),
-    nextArrow:$('.fm-banner__right-arw'),
-    dots: true,
-});
-$('.fm-testimonials__outer').slick({
-    prevArrow: $('.fm-testimonials__ltarw'),
-    nextArrow:$('.fm-testimonials__rtarw'),
-    infinite: false,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
   prevArrow: $('.fm-banner__left-arw'),
   nextArrow:$('.fm-banner__right-arw'),
   dots: true,
@@ -70,3 +35,36 @@ $('.fm-testimonials__outer').slick({
     // instead of a settings object
   ]
 });
+
+$('.fm-testimonials__outer').slick({
+    prevArrow: $('.fm-testimonials__ltarw'),
+    nextArrow:$('.fm-testimonials__rtarw'),
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+ 
